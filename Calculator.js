86202cdb19097507d9834ff9201d9b6ffd059e13
Govind Gupta <97119063+Govind789@users.noreply.github.com>
2022-08-20 01:01:25 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,21 @@
+function add(){
+    let x = document.getElementById("textval").value; 
+    let z = /[a-zA-Z@#%^&_`]/g; 
+    if (x == ""){
+        document.getElementById("textval").value = "err";
+    }
+    else if (z.test(x)){
+        document.getElementById("textval").value = "undefined";
+    }
+    else{
+        let y = eval(x)
+        document.getElementById("textval").value = y;
+    }
+}
+function display(val){
+    document.getElementById("textval").value += val;
+}
+
+function clr(){
+    document.getElementById("textval").value = "";
+}
